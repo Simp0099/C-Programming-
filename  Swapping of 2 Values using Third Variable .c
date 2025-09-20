ed0867775b2c1ae76bee5c9fe9714fed904028c6
Bhavishya <bhavishya0045@gmail.com>
2025-09-20 21:57:32 +0530

    Added

---  Swapping of 2 Values using Third Variable .c
+++  Swapping of 2 Values using Third Variable .c
@@ -5,11 +5,13 @@ int main()
     int Variable_1,Variable_2, Variable_3;
     printf ("Enter the Value for Variable 1 :");
     scanf("%d", &Variable_1);
+    
     printf ("Enter the Value for Variable 2 :");
     scanf("%d", &Variable_2);
 
     Variable_3 = Variable_1;
     Variable_1 = Variable_2;
     Variable_2 = Variable_3;
+
     printf("After Swapping :%d,%d  ",Variable_1,Variable_2);
 }
