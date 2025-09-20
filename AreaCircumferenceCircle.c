eeedf15b1fd03a88f022a3d28cf86855df3061af
Bhavishya <bhavishya0045@gmail.com>
2025-09-20 20:37:56 +0530

    Added Area Cum Circumference

--- AreaCircumferenceCircle.c
+++ AreaCircumferenceCircle.c
@@ -1,17 +1,19 @@
-//To Find the Circumference of the Circle
+//To Find the Circumference & Area of the Circle
 
 #include <stdio.h>
 int main()
 {
 
-    float radius, circumference;
+    float radius, circumference, area;
 
     printf("Enter the radius of the circle: ");
     scanf("%f", &radius);
 
     circumference = 2 * 3.14159 * radius;
+    area = 3.14159 * radius * radius;
 
     printf("The circumference of the circle for the Radius %f",radius);
     printf(" is : %f\n\n", circumference);
-
+    printf("The area of the circle for the Radius %f",radius);
+    printf(" is : %f\n", area);   
 }
