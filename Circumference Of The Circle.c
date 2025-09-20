fce1957d644174f93d4e299ff6fd6fd123fb366c
Bhavishya <bhavishya0045@gmail.com>
2025-09-20 20:30:08 +0530

    Added Circumference

---
+++ Circumference Of The Circle.c
@@ -0,0 +1,15 @@
+//To Find the Circumference of the Circle
+#include <stdio.h>
+int main()
+{
+
+    float radius, circumference;
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+    circumference = 2 * 3.14159 * radius;
+    printf("The circumference of the circle for the Radius %f",radius);
+    printf(" is : %f", circumference);
+
+
+    
+}
